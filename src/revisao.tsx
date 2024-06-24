2c7a4af7b9dff0e9d4bae28fa211ae2b3a5307a7
Guilherme Figueiredo Terenciani <guilhermeterenciani@gmail.com>
2024-06-24 10:56:07 -0400

    Utilizando valores de 2 campos de input

--- src/revisao.tsx
+++ src/revisao.tsx
@@ -1,34 +1,17 @@
 function Revisao(){
-    // Faça uma função mostraNumeros pares que receba um objeto de 
-    // Numeros que tenha as propriedades min e max
-    // E devolva uma string do número min até o max em sequencia dos números pares
-
-    // Ex:
-    /** obj.min = 10
-     *  obj.max = 15
-     *  mostraNumeros(obj)
-     *  retorno "10,12,14"
-     * 
-    */
-    type F = {
-        min:number,
-        max:number
-    }
-    function mostraNumeros(obj:F){
-        let texto = ''
-        let i = obj.min
-        while(i<=obj.max){
-            if(i%2==0){
-                texto = texto.concat(i.toString(),",")
-            }
-            i = i+1
-        }
-        return texto
-    }
-
-    let obj = {min:10,max:15}
+      /**
+	 * Transforma uma string em letras maiúsculas
+	 * @param str  A string a ser transformada em maiúsculas
+	 * @returns    A string em maiúsculas
+	 * @example
+	 * transformarParaMaiusculas('exemplo'); // retorna 'EXEMPLO'
+	*/
+	//---Adicione seu código aqui:
+    // Arrow Function
+    const transformarParaMaiusculas = (str:string)=> str.toUpperCase()
+	//-----Fim do código*/
     return(
-        <h1>{mostraNumeros(obj)}</h1>
+        <h1>{transformarParaMaiusculas('exemplo')}</h1>
     )
 }
 export default Revisao;
